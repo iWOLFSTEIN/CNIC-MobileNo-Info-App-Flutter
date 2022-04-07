32ac58726c84e797501d30463de42bea4c519eaf
iWOLFSTEIN <mtalha7274@gmail.com>
2022-04-07 22:50:48 +0500

    rewarded ad is integrated

--- lib/Screens/credit_claim_screen.dart
+++ lib/Screens/credit_claim_screen.dart
@@ -1,6 +1,9 @@
+import 'package:contact_api_info_app/Utils/alerts.dart';
+import 'package:contact_api_info_app/Utils/coins_calculator.dart';
 import 'package:contact_api_info_app/Utils/dimensions.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class CreditClaimScreen extends StatefulWidget {
   CreditClaimScreen({Key? key}) : super(key: key);
@@ -10,9 +13,34 @@ class CreditClaimScreen extends StatefulWidget {
 }
 
 class _CreditClaimScreenState extends State<CreditClaimScreen> {
+  RewardedAd? _rewardedAd;
+
+  loadRewardedAd() async {
+    await RewardedAd.load(
+        adUnitId: 'ca-app-pub-3940256099942544/5224354917',
+        request: AdRequest(),
+        rewardedAdLoadCallback: RewardedAdLoadCallback(
+          onAdLoaded: (RewardedAd ad) {
+            print('$ad loaded.');
+            // Keep a reference to the ad so you can show it later.
+            this._rewardedAd = ad;
+          },
+          onAdFailedToLoad: (LoadAdError error) {
+            print('RewardedAd failed to load: $error');
+          },
+        ));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadRewardedAd();
+  }
+
+  bool isClaimed = true;
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -40,37 +68,84 @@ class _CreditClaimScreenState extends State<CreditClaimScreen> {
                 child: GridView.count(
                   crossAxisCount: 4,
                   children: [
-                    for (int i = 0; i < 7; i++)
+                    for (int i = 1; i <= 7; i++)
                       Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          // height: 5,
-                          decoration: BoxDecoration(
-                              color: Color(0xFFFF2523),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(5))),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "DAY 1",
-                                style: GoogleFonts.roboto(
-                                    textStyle: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w300)),
+                        child: (isClaimed)
+                            ? Stack(
+                                fit: StackFit.expand,
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: Color(0xFFFF2523),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5))),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "DAY ${i}",
+                                          style: GoogleFonts.roboto(
+                                              textStyle: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w300)),
+                                        ),
+                                        Text(
+                                          "${CoinCalculator.getCoins(id: i)} COINS",
+                                          style: GoogleFonts.roboto(
+                                              textStyle: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 13,
+                                                  fontWeight: FontWeight.w300)),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                        color: Colors.black.withOpacity(0.4),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5))),
+                                    child: Center(
+                                        child: Text(
+                                      "CLAIMED",
+                                      style: GoogleFonts.roboto(
+                                          textStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 10)),
+                                    )),
+                                  ),
+                                ],
+                              )
+                            : Container(
+                                decoration: BoxDecoration(
+                                    color: Color(0xFFFF2523),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(5))),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "DAY ${i}",
+                                      style: GoogleFonts.roboto(
+                                          textStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w300)),
+                                    ),
+                                    Text(
+                                      "${CoinCalculator.getCoins(id: i)} COINS",
+                                      style: GoogleFonts.roboto(
+                                          textStyle: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.w300)),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              Text(
-                                "10 COINS",
-                                style: GoogleFonts.roboto(
-                                    textStyle: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.w300)),
-                              ),
-                            ],
-                          ),
-                        ),
                       )
                   ],
                 ),
@@ -85,7 +160,16 @@ class _CreditClaimScreenState extends State<CreditClaimScreen> {
                     color: Color(0xFFFF2523),
                     borderRadius: BorderRadius.all(Radius.circular(5))),
                 child: TextButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    if (_rewardedAd != null) {
+                      _rewardedAd!.show(
+                          onUserEarnedReward:
+                              (AdWithoutView ad, RewardItem rewardItem) {});
+                    } else {
+                      showInfoAlert(context,
+                          title: "Ad is not available currently!");
+                    }
+                  },
                   child: Text(
                     "Claim Reward",
                     style: GoogleFonts.roboto(
