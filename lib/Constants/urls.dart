b9e47936ad369806625b3a0aee1e8d82d5ee68ba
iWOLFSTEIN <mtalha7274@gmail.com>
2022-04-08 21:22:53 +0500

    settings screen is added

---
+++ lib/Constants/urls.dart
@@ -0,0 +1,4 @@
+const privacyPolicyUrl = "https://www.google.com/";
+const termsOfServiceUrl = "https://www.google.com/";
+const rateAppUrl = "https://www.google.com/";
+const contactSupportUrl = "https://www.google.com/";
---
+++ lib/Screens/settings_screen.dart
@@ -0,0 +1,102 @@
+import 'package:contact_api_info_app/Constants/urls.dart';
+import 'package:contact_api_info_app/Utils/alerts.dart';
+import 'package:contact_api_info_app/Utils/dimensions.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SettingsScreen extends StatefulWidget {
+  SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  void launchURL(context, {required url}) async {
+    try {
+      if (!await launch(url)) throw 'Could not launch $url';
+    } catch (e) {
+      print(e.toString());
+      showInfoAlert(context, title: "Network Error!");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF7F8FE),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: width(context) * 6 / 100),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+            settingButton(
+                title: "Privacy Policy",
+                voidCallBack: () {
+                  launchURL(context, url: privacyPolicyUrl);
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            settingButton(
+                title: "Terms of Service",
+                voidCallBack: () {
+                  launchURL(context, url: termsOfServiceUrl);
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            settingButton(
+                title: "Rate App",
+                voidCallBack: () {
+                  launchURL(context, url: rateAppUrl);
+                }),
+            SizedBox(
+              height: 10,
+            ),
+            settingButton(
+                title: "Contact Support",
+                voidCallBack: () {
+                  launchURL(context, url: contactSupportUrl);
+                }),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container settingButton({title, voidCallBack}) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.black.withOpacity(0.075)),
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: TextButton(
+        onPressed: voidCallBack,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              title,
+              style: GoogleFonts.roboto(
+                  textStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
